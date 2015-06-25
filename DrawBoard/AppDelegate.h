a5189e5f17e44bc5039a8e6e14ad3e8882ddc131
IceAgeQQ <d830300042@gmail.com>
2015-06-24 18:23:53 -0700

    first commit DrawBoard

---
+++ DrawBoard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DrawBoard
+//
+//  Created by Chao Xu on 15/6/24.
+//  Copyright (c) 2015年 Chao Xu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DrawBoard/ViewController.h
@@ -0,0 +1,35 @@
+//
+//  ViewController.h
+//  DrawBoard
+//
+//  Created by Chao Xu on 15/6/24.
+//  Copyright (c) 2015年 Chao Xu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController {
+    
+    //lastPoint stores the last drawn point on the canvas
+    //red, blue, and green store the current RGB values of the selected color.
+    
+    CGPoint lastPoint;
+    CGFloat red;
+    CGFloat green;
+    CGFloat blue;
+    CGFloat brush;
+    CGFloat opacity;
+    BOOL mouseSwiped;
+}
+@property (strong,nonatomic) IBOutlet UIImageView *mainImage;
+
+@property (strong, nonatomic) IBOutlet UIImageView *tempDrawImage;
+
+- (IBAction)pencilPressed:(id)sender;
+- (IBAction)eraserPressed:(id)sender;
+- (IBAction)reset:(id)sender;
+- (IBAction)save:(id)sender;
+- (IBAction)settings:(id)sender;
+
+@end
+
